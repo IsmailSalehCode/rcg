5e6e646a7f37b047f8d596a386297a22d7a512d7
Ismail Saleh <centriu78@gmail.com>
2024-12-15 00:08:02 +0200

    to Composition API

--- src/views/RCGHexColorValue.vue
+++ src/views/RCGHexColorValue.vue
@@ -5,49 +5,86 @@
     >{{ outputHexColorStr }}</span
   >
 </template>
-<script>
+<script setup lang="ts">
+import { ref, computed, onMounted, watch } from "vue";
 import { Queue } from "@/utils/Queue";
 
-export default {
-  mounted() {
-    this.outputHexColorStr = this.currentNumsHexColorStr;
+const { nums, isDelayed } = defineProps({
+  nums: {
+    type: Array<number>,
+    required: true,
   },
-  props: {
-    nums: {
-      type: Array,
-      required: true,
-    },
-    isDelayed: {
-      type: Boolean,
-      default: false,
-    },
+  isDelayed: {
+    type: Boolean,
+    default: false,
   },
-  data() {
-    return {
-      delayQueue: new Queue(),
-      outputHexColorStr: null,
-    };
-  },
-  watch: {
-    nums() {
-      if (!this.isDelayed) {
-        this.delayQueue.clear();
-        this.outputHexColorStr = this.currentNumsHexColorStr;
-      } else {
-        this.delayQueue.enqueue(this.currentNumsHexColorStr);
-        setTimeout(() => {
-          this.outputHexColorStr = this.delayQueue.dequeue();
-        }, 3000);
-      }
-    },
-  },
-  computed: {
-    currentNumsHexColorStr() {
-      const toHex = (value) => value.toString(16).padStart(2, "0");
-      return `#${toHex(this.nums[0])}${toHex(this.nums[1])}${toHex(
-        this.nums[2]
-      )}`;
-    },
-  },
+});
+
+const delayQueue = new Queue<string>();
+const outputHexColorStr = ref<string | null>(null);
+
+const currentNumsHexColorStr = computed(() => {
+  const toHex = (value: number) => value.toString(16).padStart(2, "0");
+  return `#${toHex(nums[0])}${toHex(nums[1])}${toHex(nums[2])}`;
+});
+
+const updateOutput = () => {
+  if (!isDelayed) {
+    delayQueue.clear();
+    outputHexColorStr.value = currentNumsHexColorStr.value;
+  } else {
+    delayQueue.enqueue(currentNumsHexColorStr.value);
+    setTimeout(() => {
+      outputHexColorStr.value = delayQueue.dequeue();
+    }, 3000);
+  }
 };
+
+watch(() => nums, updateOutput);
+
+onMounted(() => {
+  outputHexColorStr.value = currentNumsHexColorStr.value;
+});
+// export default {
+//   mounted() {
+//     this.outputHexColorStr = this.currentNumsHexColorStr;
+//   },
+//   props: {
+//     nums: {
+//       type: Array,
+//       required: true,
+//     },
+//     isDelayed: {
+//       type: Boolean,
+//       default: false,
+//     },
+//   },
+//   data() {
+//     return {
+//       delayQueue: new Queue(),
+//       outputHexColorStr: null,
+//     };
+//   },
+//   watch: {
+//     nums() {
+//       if (!this.isDelayed) {
+//         this.delayQueue.clear();
+//         this.outputHexColorStr = this.currentNumsHexColorStr;
+//       } else {
+//         this.delayQueue.enqueue(this.currentNumsHexColorStr);
+//         setTimeout(() => {
+//           this.outputHexColorStr = this.delayQueue.dequeue();
+//         }, 3000);
+//       }
+//     },
+//   },
+//   computed: {
+//     currentNumsHexColorStr() {
+//       const toHex = (value) => value.toString(16).padStart(2, "0");
+//       return `#${toHex(this.nums[0])}${toHex(this.nums[1])}${toHex(
+//         this.nums[2]
+//       )}`;
+//     },
+//   },
+// };
 </script>
